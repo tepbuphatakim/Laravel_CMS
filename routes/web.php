e116c2a23cfd4cadda7ad8bbd018493e6a85c8af
Kim Tepbuphata <kimtepbuphata009@gmail.com>
2022-08-18 22:04:22 +0700

    feat(web-route): welcome

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('product.index');
+    return redirect()->route('welcome');
 });
 
 Route::group(['middleware' => 'auth'], function () {
