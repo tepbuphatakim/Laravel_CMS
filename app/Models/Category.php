058327520b595eb16fa3f048855a604818e20f74
Kim Tepbuphata <kimtepbuphata009@gmail.com>
2022-08-18 21:58:36 +0700

    feat(article-relation): add relation tags, category to article

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\Article;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     protected $table = 'categories';
     protected $fillable = ['title'];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
